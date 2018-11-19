fdb3a9fba36ec71e770e251bf720a65b3fa91cda
Anirudh <kanirudh54@gmail.com>
2018-11-19 01:36:17 +0000

    Basic Structure

---
+++ server/kvstore.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"log"
+
+	context "golang.org/x/net/context"
+
+	"github.com/nyu-distributed-systems-fa18/distributed-trie/pb"
+)
+
+// The struct for data to send over channel
+type InputChannelType struct {
+	command  pb.Command
+	response chan pb.Result
+}
+
+// The struct for key value stores.
+type KVStore struct {
+	C     chan InputChannelType
+	store map[string]int64
+}
+
+func (s *KVStore) Get(ctx context.Context, key *pb.Key) (*pb.Result, error) {
+	// Create a channel
+	c := make(chan pb.Result)
+	// Create a request
+	r := pb.Command{Operation: pb.Op_GET, Arg: &pb.Command_Get{Get: key}}
+	// Send request over the channel
+	s.C <- InputChannelType{command: r, response: c}
+	log.Printf("Waiting for get response")
+	result := <-c
+	// The bit below works because Go maps return the 0 value for non existent keys, which is empty in this case.
+	return &result, nil
+}
+
+func (s *KVStore) Set(ctx context.Context, in *pb.Key) (*pb.Result, error) {
+	// Create a channel
+	c := make(chan pb.Result)
+	// Create a request
+	r := pb.Command{Operation: pb.Op_SET, Arg: &pb.Command_Set{Set: in}}
+	// Send request over the channel
+	s.C <- InputChannelType{command: r, response: c}
+	log.Printf("Waiting for set response")
+	result := <-c
+	// The bit below works because Go maps return the 0 value for non existent keys, which is empty in this case.
+	return &result, nil
+}
+
+func (s *KVStore) Clear(ctx context.Context, in *pb.Empty) (*pb.Result, error) {
+	// Create a channel
+	c := make(chan pb.Result)
+	// Create a request
+	r := pb.Command{Operation: pb.Op_CLEAR, Arg: &pb.Command_Clear{Clear: in}}
+	// Send request over the channel
+	s.C <- InputChannelType{command: r, response: c}
+	log.Printf("Waiting for clear response")
+	result := <-c
+	// The bit below works because Go maps return the 0 value for non existent keys, which is empty in this case.
+	return &result, nil
+}
+
+
+// Used internally to generate a result for a get request. This function assumes that it is called from a single thread of
+// execution, and hence does not handle races.
+func (s *KVStore) GetInternal(k string) pb.Result {
+	v := s.store[k]
+	return pb.Result{Result: &pb.Result_Kv{Kv: &pb.KeyValue{Key: k, Value: v}}}
+}
+
+// Used internally to set and generate an appropriate result. This function assumes that it is called from a single
+// thread of execution and hence does not handle race conditions.
+func (s *KVStore) SetInternal(k string) pb.Result {
+	s.store[k] = s.store[k] + 1
+	return pb.Result{Result: &pb.Result_Kv{Kv: &pb.KeyValue{Key: k, Value: s.store[k]}}}
+}
+
+// Used internally, this function clears a kv store. Assumes no racing calls.
+func (s *KVStore) ClearInternal() pb.Result {
+	s.store = make(map[string]int64)
+	return pb.Result{Result: &pb.Result_S{S: &pb.Success{}}}
+}
+
+
+func (s *KVStore) HandleCommand(op InputChannelType) {
+	switch c := op.command; c.Operation {
+	case pb.Op_GET:
+		arg := c.GetGet()
+		result := s.GetInternal(arg.Key)
+		op.response <- result
+	case pb.Op_SET:
+		arg := c.GetSet()
+		result := s.SetInternal(arg.Key)
+		op.response <- result
+	case pb.Op_CLEAR:
+		result := s.ClearInternal()
+		op.response <- result
+	default:
+		// Sending a blank response to just free things up, but we don't know how to make progress here.
+		op.response <- pb.Result{}
+		log.Fatalf("Unrecognized operation %v", c)
+	}
+}
+
+// Handle command for followers to change state. Its input will only have command
+func (s *KVStore) HandleCommandSecondary(op pb.Command) {
+	switch c := op; c.Operation {
+	case pb.Op_GET:
+		arg := c.GetGet()
+		s.GetInternal(arg.Key)
+	case pb.Op_SET:
+		arg := c.GetSet()
+		s.SetInternal(arg.Key)
+	case pb.Op_CLEAR:
+		s.ClearInternal()
+	default:
+		// Sending a blank response to just free things up, but we don't know how to make progress here.
+		log.Fatalf("Unrecognized operation %v", c)
+	}
+}
---
+++ server/serve.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nyu-distributed-systems-fa18/distributed-trie/pb"
+	context "golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+	rand "math/rand"
+	"net"
+	"time"
+)
+
+//
+const (
+	Primary = "primary"
+	Secondary = "secondary"
+)
+
+// Messages that can be passed from the Raft RPC server to the main loop for AppendEntries
+type RequestArgs struct {
+	arg      *pb.UpdateSecondaryTrieRequest
+}
+
+// Messages that can be passed from the Raft RPC server to the main loop for VoteInput
+type ReplyArgs struct {
+	arg      *pb.UpdateSecondaryTrieReply
+}
+
+type ControlArgs struct {
+	arg *pb.ControlRequest
+}
+
+// Struct off of which we shall hang the Raft service
+type Repl struct {
+	ControlChan chan ControlArgs
+	RequestChan chan RequestArgs
+	ReplyChan   chan ReplyArgs
+}
+
+func (r *Repl) Init(ctx context.Context, arg *pb.ControlRequest) (*pb.Empty, error) {
+	r.ControlChan <- ControlArgs{arg: arg}
+	return &pb.Empty{}, nil
+}
+
+func (r *Repl) UpdateSecondary(ctx context.Context, arg *pb.UpdateSecondaryTrieRequest) (*pb.Empty, error) {
+	r.RequestChan <- RequestArgs{arg: arg}
+	return &pb.Empty{}, nil
+}
+
+func (r *Repl) AckPrimary(ctx context.Context, arg *pb.UpdateSecondaryTrieReply ) (*pb.Empty, error) {
+	r.ReplyChan <- ReplyArgs{arg: arg}
+	return &pb.Empty{}, nil
+}
+
+// Compute a random duration in milliseconds
+func randomDuration(r *rand.Rand) time.Duration {
+	// Constant
+	const DurationMax = 50000 //4000
+	const DurationMin = 25000 //1000
+	return time.Duration(r.Intn(DurationMax-DurationMin)+DurationMin) * time.Millisecond
+}
+
+// Restart the supplied timer using a random timeout based on function above
+func restartTimer(timer *time.Timer, r *rand.Rand) {
+	stopped := timer.Stop()
+	// If stopped is false that means someone stopped before us, which could be due to the timer going off before this,
+	// in which case we just drain notifications.
+	if !stopped {
+		// Loop for any queued notifications
+		for len(timer.C) > 0 {
+			<-timer.C
+		}
+
+	}
+	timer.Reset(randomDuration(r))
+}
+
+// Launch a GRPC service for this Repl peer.
+func RunReplServer(r *Repl, port int) {
+	// Convert port to a string form
+	portString := fmt.Sprintf(":%d", port)
+	// Create socket that listens on the supplied port
+	c, err := net.Listen("tcp", portString)
+	if err != nil {
+		// Note the use of Fatalf which will exit the program after reporting the error.
+		log.Fatalf("Could not create listening socket %v", err)
+	}
+	// Create a new GRPC server
+	s := grpc.NewServer()
+
+	pb.RegisterReplServer(s, r)
+	log.Printf("Going to listen on port %v", port)
+
+	// Start serving, this will block this function and only return when done.
+	if err := s.Serve(c); err != nil {
+		log.Fatalf("Failed to serve %v", err)
+	}
+}
+
+func connectToPeer(peer string) (pb.ReplClient, error) {
+	backoffConfig := grpc.DefaultBackoffConfig
+	// Choose an aggressive backoff strategy here.
+	backoffConfig.MaxDelay = 500 * time.Millisecond
+	conn, err := grpc.Dial(peer, grpc.WithInsecure(), grpc.WithBackoffConfig(backoffConfig))
+	// Ensure connection did not fail, which should not happen since this happens in the background
+	if err != nil {
+		return pb.NewReplClient(nil), err
+	}
+	return pb.NewReplClient(conn), nil
+}
+
+// The main service loop. All modifications to the Trie are run through here.
+func serve(s *KVStore, r *rand.Rand, peers *arrayPeers, id string, port int) {
+	repl := Repl{RequestChan: make(chan RequestArgs), ReplyChan: make(chan ReplyArgs)}
+	// Start in a Go routine so it doesn't affect us.
+	go RunReplServer(&repl, port)
+
+	peerClients := make(map[string]pb.ReplClient)
+
+	// Peer state
+	type PeerState struct {
+		requests map[int64]string
+	}
+	// Current node's state
+	type SelfState struct {
+		completed map[int64]bool
+	}
+
+	peerStates := make(map[string] *PeerState)
+	selfState := SelfState{make(map[int64]bool)}
+	for _, peer := range *peers {
+		client, err := connectToPeer(peer)
+		if err != nil {
+			log.Fatalf("Failed to connect to GRPC server %v", err)
+		}
+		peerStates[peer] = &PeerState{make(map[int64]string)}
+		peerClients[peer] = client
+		log.Printf("Connected to %v", peer)
+	}
+	var role = Secondary
+	var requestNumber int64 = 0
+	var primaryId string = ""
+	// Create a timer and start running it
+	timer := time.NewTimer(randomDuration(r))
+
+	// Run forever handling inputs from various channels
+	for {
+		for k, v := range peerStates {
+			log.Printf("Peer: %v", k)
+			for r,w := range v.requests{
+				log.Printf("Req: %v , Word: %v", r, w)
+			}
+		}
+		for k,v := range selfState.completed {
+			log.Printf("Req: %v , Success: %v", k, v)
+		}
+		select {
+		case <-timer.C:
+			log.Printf("timer off")
+			restartTimer(timer, r)
+		case op := <-s.C:
+			// Call handle command for op
+			go s.HandleCommand(op)
+			// Do replication if Set - Update
+			log.Printf("Op is %v", op.command.GetOperation())
+			if op.command.GetOperation() == 1 { //set
+				requestNumber += 1
+				for p, c := range peerClients {
+					word :=  op.command.GetSet().GetKey()
+					peerStates[p].requests[requestNumber] = word
+					go func(c pb.ReplClient, p string) {
+						c.UpdateSecondary(context.Background(), &pb.UpdateSecondaryTrieRequest{Word:word, RequestNumber:requestNumber})
+					}(c, p)
+					log.Printf("Sent ReplicateReq (%v,%v) to %v", requestNumber, word, p)
+				}
+			}
+		case req := <-repl.RequestChan:
+			// replicate and update selfstate
+			if _, ok:= selfState.completed[req.arg.GetRequestNumber()]; role == Secondary && !ok {//TODO: deal with concurrent Trie requests
+				selfState.completed[req.arg.GetRequestNumber()] = true
+				go s.HandleCommandSecondary( pb.Command{Operation: pb.Op_SET, Arg: &pb.Command_Set{Set: &pb.Key{Key: req.arg.GetWord()}}})
+				peerClients[primaryId].AckPrimary(context.Background(), &pb.UpdateSecondaryTrieReply{RequestNumber: req.arg.GetRequestNumber(), Success : true, Peer : id})
+				log.Printf("Sending Ack to primary %v from %v", primaryId, id)
+			}
+		case rep := <-repl.ReplyChan:
+			if rep.arg.GetSuccess() {
+				delete (peerStates[rep.arg.GetPeer()].requests, rep.arg.GetRequestNumber())
+				log.Printf("Replicated request %v on peer %v", rep.arg.GetRequestNumber(), rep.arg.GetPeer())
+			}
+
+		case con := <-repl.ControlChan:
+			if con.arg.GetPrimaryId() == id {
+				//TODO: reset Table
+				log.Printf("I am Primary. My id is %v", id)
+				requestNumber = con.arg.GetRequestNumber()
+				role = Primary
+			} else { //TODO: reset the tables
+				role = Secondary
+				primaryId = con.arg.GetPrimaryId()
+				log.Printf("I am Secondary. My id is %v and my Primary is %v", id, primaryId)
+			}
+		}
+	}
+	log.Printf("Strange to arrive here")
+}
